367de3c6dbf365424b766ed63b2399b52ad2cdb7
zzzqqqqhhh <1205359255@qq.com>
2019-12-20 14:08:36 +0800

    Django's addition, deletion, modification and search

--- front/models.py
+++ front/models.py
@@ -6,3 +6,10 @@ class Users(models.Model):
     name = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     price = models.FloatField(default=0)
+
+    def __str__(self):
+        return "<Users:({id},{name},{author},{price})>".format(id=self.id,name=self.name,author=self.author,price=self.price)
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100,null=False)
+    address = models.CharField(max_length=100, null=False)
--- front/views.py
+++ front/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .models import Users,Publisher
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    #插入
+    # user = Users(name='三国演义',author='Tom',price=200)
+    # user.save()
+    #删除
+    # user = Users(id=1)
+    # user.delete()
+    #更新
+    # user = Users.objects.get(pk=2)
+    # user.price = 5000
+    # user.save()
+    #1.1查询(根据主键查询)
+    # print(Users.objects.get(pk=2))
+    #1.2查询(根据条件查找)
+    # print(Users.objects.filter(author='Tom').first())
+
+    return HttpResponse("三国演义")
